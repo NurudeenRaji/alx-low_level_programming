ff98bcaccce0e0a3218085d5cbc5bb0da5606124
Raji Nurudeen <orizon.bot21@gmail.com>
2023-09-07 20:27:58 +0000

    Hello world task

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,15 @@
 #include <stdio.h>
 
-/*
+/**
  * main - The entry point of the program
+ *
+ * Return: Always 0 (Success).
  */
 
 int main(void)
 {
 	char puzzle[] = "\"Programming is like building a multilingual puzzle";
+
 	puts(puzzle);
 
 	return (0);
