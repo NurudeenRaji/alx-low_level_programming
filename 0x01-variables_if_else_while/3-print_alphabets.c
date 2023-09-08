2de6626e562220b8218d77414963b62bac56782a
Raji Nurudeen <orizon.bot21@gmail.com>
2023-09-08 12:16:12 +0000

    0x01. C Task

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Program entry point.
+ *
+ * Return: 0(Success), 1(Error).
+ */
+
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+		putchar(c);
+	for (c = 'A'; c <= 'Z'; c++)
+		putchar(c);
+	putchar('\n');
+
+	return (0);
+}
