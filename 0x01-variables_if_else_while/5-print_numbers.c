f4721144f493a26cde35c6eb50cc7d095fc6b4cd
Raji Nurudeen <orizon.bot21@gmail.com>
2023-09-08 13:35:38 +0000

    0x01. C Task

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Program enntry point.
+ *
+ * Return: 0(Success), 1(Error).
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+		printf("%d", i);
+	printf("\n");
+}
