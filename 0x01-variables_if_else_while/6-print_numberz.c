f478f03a0c2400971bd9d08258e043919c9be654
Raji Nurudeen <orizon.bot21@gmail.com>
2023-09-08 13:49:03 +0000

    0x01. C Task

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Program enttry point
+ *
+ * Return: 0(Success), 1(Error).
+ */
+
+int main(void)
+{
+	int c;
+
+	for (c = '0'; c <= '9'; c++)
+		putchar(c);
+	putchar('\n');
+
+	return (0);
+}
