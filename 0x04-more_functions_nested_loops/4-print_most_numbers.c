38882e17db0826bb4bbf2a51dd05258bd3a9d82e
Raji Nurudeen <orizon.bot21@gmail.com>
2023-09-14 17:18:29 +0000

    0x04. C - More functions

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_most_numbers - Print nnumbers except 2 and 4.
+ */
+
+void print_most_numbers(void)
+{
+	char c;
+
+	for (c = 48; c < 58; c++)
+	{
+		if (c == 50 || c == 52)
+			continue;
+		else
+			_putchar(c);
+	}
+	_putchar('\n');
+}
+
+int main(void)
+{
+	print_most_numbers();
+	return (0);
+}
