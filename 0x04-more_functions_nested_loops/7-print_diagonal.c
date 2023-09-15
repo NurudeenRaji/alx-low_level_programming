e1aeb9ea04055964a7daeb4f429aaa8ed8a0ba4e
Raji Nurudeen <orizon.bot21@gmail.com>
2023-09-15 06:08:56 +0000

    0x04. C - More functions

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -8,6 +8,29 @@
 
 void print_diagonal(int n)
 {
-	int i;
+	int i, j;
 
-	for (
+	for (i = 1; i <= n; i++)
+	{
+		if (n > 0)
+		{
+			for (j = 1; j < i; j++)
+				_putchar(' ');
+			_putchar('\\');
+			_putchar('\n');
+		}
+		else
+		{
+			_putchar('\n');
+		}
+	}
+}
+
+int main(void)
+{
+	print_diagonal(0);
+	print_diagonal(2);
+	print_diagonal(10);
+	print_diagonal(-4);
+	return (0);
+}
