98a406f2874e0f3fc1f3d5d8eaaaeeca1b825752
NurudeenRaji <orizon.bot21@gmail.com>
2023-09-21 07:12:01 +0000

    0x06. C - More pointers, arrays and strings

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -11,26 +11,35 @@
 
 char *_strncat(char *dest, char  *src, int n)
 {
-	int i, len1, len2, j, k;
+	int i, len1, k;
+	int j, len2;
 
+	/* Calculating the length of string 'dest' */
 	for (i = 0; dest[i] != '\0'; i++)
 	{
 	}
 	len1 =  i;
+
+	/* Calculating the length of string 'dest' */
 	for (j = 0; src[j] != '\0'; j++)
 	{
 	}
 	len2 = j;
 
+	/* Appending the specified number from src to dest */
 	for (k = 0; k < n; k++)
 	{
-		dest[len1 + k] = src[k];
+		if (src[k] != '\0') /*Incase 'src' is null*/
+			dest[len1 + k] = src[k];
+		else
+			break;
 	}
 
+	/* Reallocating the memory to end with null */
 	if (n < len2)
 		dest[len1 + n] = '\0';
 	else
-		dest[len1 + len2] = '\0';
+		dest[len1 + len2] = '\0'; /*Incase n is greater  than size of dest */
 
 	return (dest);
 }
