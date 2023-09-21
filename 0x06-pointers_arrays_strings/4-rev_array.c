12b2d3c5c83a1af8d42bff3340ba75da8ad06df9
NurudeenRaji <orizon.bot21@gmail.com>
2023-09-21 22:40:02 +0000

    0x06. C - More pointers, arrays and strings

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * reverse_array - Reverses the content of an array.
+ * @a: String to reverse
+ * @n: the number of array
+ */
+
+void reverse_array(int *a, int n)
+{
+	int j;
+	char c;
+
+	for (j = 0; j < (n / 2); j++)
+	{
+		c = a[j];
+		a[j] = a[n - 1 - j];
+		a[n - 1 - j] = c;
+	}
+}
