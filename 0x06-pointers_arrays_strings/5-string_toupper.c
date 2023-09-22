096285d9736585880f01427bff5cda03a161e81f
NurudeenRaji <orizon.bot21@gmail.com>
2023-09-22 05:28:37 +0000

    0x06. C - More pointers, arrays and strings

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -2,4 +2,22 @@
 #include <stdio.h>
 
 /**
- *
+ * string_toupper - Changes lowercase to uppercase.
+ * @str: string to convert.
+ * Return: the char changed.
+ */
+
+char *string_toupper(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (str[i] >= 97 && str[i] <= 122)
+		{
+			str[i] = str[i] - 32;
+		}
+	}
+
+	return (str);
+}
