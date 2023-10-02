01adfe16a4a965b97602a90fea479eabe83cc1a9
Raji Nurudeen <orizon.bot21@gmail.com>
2023-10-02 23:34:46 +0100

    0x09. C - Static libraries

---
+++ 0x09-static_libraries/0-isupper.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * _isupper - Checks uppercase
+ * @c: character
+ * Return: 1 or 0.
+ */
+
+int _isupper(int c)
+{
+}
---
+++ 0x09-static_libraries/0-memset.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _memset - memory set.
+ * @s: sttring.
+ * @src: source code.
+ * @n: number.
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+}
