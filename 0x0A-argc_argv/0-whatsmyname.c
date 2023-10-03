815a0f30e49fcef99572753230f70f20c6da886f
Raji Nurudeen <orizon.bot21@gmail.com>
2023-10-03 17:47:06 +0100

    0x0A. C - argc, argv

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,5 +1,12 @@
 #include <stdio.h>
 
+/**
+ * main - Entry point.
+ * @argc: argument count.
+ * @argv: argument array.
+ * Return: 0 (Success), 1 (Error).
+ */
+
 int main(int argc, char *argv[])
 {
 	printf("%s\n", argv[0]);
