c85249536ccf2eaab2cd4674954cc064c67a21d0
Raji Nurudeen <orizon.bot21@gmail.com>
2023-10-03 18:34:28 +0100

    0x0A. C - argc, argv

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,5 +1,12 @@
 #include <stdio.h>
 
+/**
+ * main - Entry point.
+ * @argc: Argument count.
+ * @argv: Argument array.
+ * Return: 0 (Success), 1(Error).
+ */
+
 int main(int argc, __attribute__((unused))char *argv[])
 {
 	int i;
