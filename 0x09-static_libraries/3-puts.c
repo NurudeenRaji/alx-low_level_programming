140a9901d833c241e773195a9a53530734baf367
Raji Nurudeen <orizon.bot21@gmail.com>
2023-10-03 01:54:10 +0100

    0x09. C - Static libraries

--- 0x09-static_libraries/3-puts.c
+++ 0x09-static_libraries/3-puts.c
@@ -1,5 +1,21 @@
 #include "main.h"
+#include <stdio.h>
 
-void _puts(char *s)
+/**
+ * _puts - Prints a string
+ * @str: the string.
+ */
+
+void _puts(char *str)
 {
+	int i;
+
+	i = 0;
+	while (*str != '\0')
+	{
+		_putchar(*str);
+		str++;
+		i++;
+	}
+	_putchar('\n');
 }
