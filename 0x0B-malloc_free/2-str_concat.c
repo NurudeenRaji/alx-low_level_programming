9a0c76f380e6511d446c25af80813a536610201b
Raji Nurudeen <orizon.bot21@gmail.com>
2023-10-06 04:45:38 +0100

    0x0B. C - malloc, free

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -12,35 +12,42 @@
 char *str_concat(char  *s1, char *s2)
 {
 	char *mem;
-	int len1, len2;
-	int i;
+	int len1, len2, i;
 
 	if (s1 == NULL && s2 == NULL)
 	{
 		return (NULL);
 	}
-
 	len1 = 0;
-	while (s1[len1] != '\0')
-		len1++;
+	if (s1 !=  NULL)
+	{
+		while (s1[len1] != '\0')
+			len1++;
+	}
 	len2 = 0;
-	while (s2[len2] != '\0')
-		len2++;
-
+	if (s2 != NULL)
+	{
+		while (s2[len2] != '\0')
+			len2++;
+	}
 	mem = malloc(sizeof(char) * (len1 + len2 + 1));
 
 	if (mem == NULL)
 		return (NULL);
-
-	for (i = 0; i < len1; i++)
+	if (s1 != NULL)
 	{
-		mem[i] = s1[i];
+		for (i = 0; i < len1; i++)
+		{
+			mem[i] = s1[i];
+		}
 	}
-	for (i = 0; i < len2; i++)
+	if (s2 != NULL)
 	{
-		mem[i + len1] = s2[i];
+		for (i = 0; i < len2; i++)
+		{
+			mem[i + len1] = s2[i];
+		}
 	}
 	mem[len1 + len2] = '\0';
-
 	return (mem);
 }
---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * alloc_grid - allocates memory for a two-dimensional array.
+ * @width - the width or row size..
+ * @height: height or column size.
+ * Return: pointer to array or NULL.
+ */
+
+int **alloc_grid(int width, int height)
+{
