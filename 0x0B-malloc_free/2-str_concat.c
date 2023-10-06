48d2fdeaccf215b61ac91fe3595920a524bcdc73
Raji Nurudeen <orizon.bot21@gmail.com>
2023-10-06 04:05:30 +0100

    0x0B. C - malloc, free

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -15,25 +15,49 @@ char *str_concat(char  *s1, char *s2)
 	int len1, len2;
 	int i;
 
-	/* Length of s1 */
-	len1 = 0;
-	while (s1[len1] != '\0')
-		len1++;
-
-	/* Length of s2 */
-	len2 = 0;
-	while (s2[len2] != '\0')
-		len2++;
-
-	mem = malloc(sizeof(*mem) * (len1 + len2 + 1));
-
-	if (mem == NULL)
-		return (NULL);
-
 	if (s1 == NULL && s2 == NULL)
 	{
 		return (NULL);
 	}
+
+	if (s1 == NULL && s2 != NULL)
+	{
+		len2 = 0;
+		while (s2[len2] != '\0')
+			len2++;
+		mem = malloc(sizeof(char) * (len2 + 1));
+		for (i = 0; i < len2; i++)
+		{
+			mem[i] = s2[i];
+		}
+		return (mem);
+	}
+
+
+	if (s1 != NULL && s2 == NULL)
+	{
+		len1 = 0;
+		while (s1[len1] != '\0')
+			len1++;
+		mem = malloc(sizeof(char) * (len1 + 1));
+		for (i = 0; i < len1; i++)
+		{
+			mem[i] = s1[i];
+		}
+		return (mem);
+	}
+
+	len1 = 0;
+	while (s1[len1] != '\0')
+		len1++;
+	len2 = 0;
+	while (s2[len2] != '\0')
+		len2++;
+
+	mem = malloc(sizeof(char) * (len1 + len2 + 1));
+
+	if (mem == NULL)
+		return (NULL);
 
 	for (i = 0; i < len1; i++)
 	{
