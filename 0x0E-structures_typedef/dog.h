600fa16e6e844d4d94fc8d23caaae4f6cb0a14fd
Raji Nurudeen <orizon.bot21@gmail.com>
2023-10-10 18:23:16 +0100

    0x0E. C - Structures, typedef

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,13 @@
 #ifndef HEADER_H
 #define HEADER_H
 
+/**
+ * struct dog - Brief description of a dog.
+ * @name: the dog's name.
+ * @age: the dog's age.
+ * @owner: the dog's owner.
+ */
+
 struct dog
 {
 	char *name;
