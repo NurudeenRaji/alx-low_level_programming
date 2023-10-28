240a6ca1fa4a01b69ada8e6abf65068773969213
Raji Nurudeen <orizon.bot21@gmail.com>
2023-10-28 22:41:55 +0100

    0x14. C - Bit manipulation

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -9,9 +9,12 @@
 
 int get_bit(unsigned long int n, unsigned int index)
 {
-	unsigned long int mask;
+	unsigned long int bit, mask;
 
 	if (n == 0)
+		return (-1);
+
+	if (index >= bit)
 		return (-1);
 
 	mask = 1UL << index;
