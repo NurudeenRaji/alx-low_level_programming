37d6e53c76d11b872cfb7dcde12516d135a5fd84
Raji Nurudeen <orizon.bot21@gmail.com>
2023-12-17 21:55:44 +0100

    0x17. C - Doubly linked lists

---
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -0,0 +1,50 @@
+#include "lists.h"
+
+/**
+ * insert_dnodeint_at_index - inserts a node at a index.
+ * @h: pointer to the head.
+ * @idx: index to insert to.
+ * @n: value to insert.
+ * Return: address of the new node.
+ */
+
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
+{
+	dlistint_t *temp, *newNode;
+	unsigned int i;
+
+	newNode = malloc(sizeof(dlistint_t));
+	newNode->n = n;
+	if (newNode == NULL)
+		return (NULL);
+
+	if (idx == 0)
+	{
+		if (*h != NULL)
+		{
+			newNode->next = *h;
+			(*h)->prev = newNode;
+		}
+		*h = newNode;
+	}
+	else
+	{
+		temp = *h;
+		i = 0;
+		while (temp != NULL && i < idx)
+		{
+			temp = temp->next;
+			i++;
+		}
+		if (temp == NULL)
+		{
+			free(newNode);
+			return (NULL);
+		}
+		newNode->next = temp;
+		newNode->prev = temp->prev;
+		temp->prev->next = newNode;
+		temp->prev = newNode;
+	}
+	return (newNode);
+}
