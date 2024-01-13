5446a6ae7cb9aaaf5a7f03862dc82622e76285e3
Raji Nurudeen <orizon.bot21@gmail.com>
2024-01-13 21:26:39 +0100

    0x1A. C - Hash tables

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -19,10 +19,11 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 	index = key_index((const unsigned char *)key, size);
 
 	node = ht->array[index];
-	if (node != NULL)
+	while (node != NULL)
 	{
 		if (strcmp(node->key, key) == 0)
 			return (node->value);
+		node = node->next;
 	}
 	return (NULL);
 }
