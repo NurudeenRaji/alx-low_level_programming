503e2a3089658bf9e497197c5be88666d16df706
Raji Nurudeen <orizon.bot21@gmail.com>
2024-01-13 18:05:54 +0100

    0x1A. C - Hash tables

---
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -0,0 +1,28 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_get - Retrieves a value with a key
+ * @ht: the hash table to retrieve a value from.
+ * @key: the key of the hash table.
+ * Return: NULL
+ */
+
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	unsigned long int size;
+	int index;
+	hash_node_t *node;
+
+	if (ht == NULL || key == NULL)
+		return (NULL);
+	size = ht->size;
+	index = key_index((const unsigned char *)key, size);
+
+	node = ht->array[index];
+	if (node != NULL)
+	{
+		if (strcmp(node->key, key) == 0)
+			return (node->value);
+	}
+	return (NULL);
+}
