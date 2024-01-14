e8f0902a97c027c56845a3e1a3f4c4ea0c47aeb1
Raji Nurudeen <orizon.bot21@gmail.com>
2024-01-14 12:08:08 +0100

    0x1A. C - Hash tables

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -1,5 +1,10 @@
 #include "hash_tables.h"
 
+/**
+ * hash_table_delete - deletes a hash table
+ * @ht: the hash table
+ */
+
 void hash_table_delete(hash_table_t *ht)
 {
 	unsigned long int i, size;
@@ -8,18 +13,32 @@ void hash_table_delete(hash_table_t *ht)
 	if (ht == NULL)
 		return;
 	size = ht->size;
-	for (i = 0; i < (size - 1); i++)
+	for (i = 0; i < size; i++)
 	{
 		node = ht->array[i];
 		while (node != NULL)
 		{
 			temp = node;
 			node = node->next;
-			free(temp->key);
-			free(temp->value);
+			if (temp->key != NULL)
+			{
+				free(temp->key);
+				temp->key = NULL;
+			}
+			if (temp->value != NULL)
+			{
+				free(temp->value);
+				temp->value = NULL;
+			}
 			free(temp);
+			temp = NULL;
 		}
 	}
-	free(ht->array);
+	if (ht->array != NULL)
+	{
+		free(ht->array);
+		ht->array = NULL;
+	}
 	free(ht);
+	ht = NULL;
 }
