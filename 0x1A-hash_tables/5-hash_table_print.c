28a6150c306f36354d67d09a2226eb4a02a31f00
Raji Nurudeen <orizon.bot21@gmail.com>
2024-01-14 11:02:23 +0100

    0x1A. C - Hash tables

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -9,12 +9,12 @@ void hash_table_print(const hash_table_t *ht)
 {
 	unsigned long int i, size;
 	hash_node_t *node;
-	int comma;
+	bool pair;
 
-	comma = 0;
-	printf("{");
+	pair = false;
 	if (ht == NULL)
 		return;
+	printf("{");
 	size = ht->size;
 
 	for (i = 0; i < size; i++)
@@ -22,16 +22,12 @@ void hash_table_print(const hash_table_t *ht)
 		node = ht->array[i];
 		while (node != NULL)
 		{
-			printf("'%s': '%s'", node->key, node->value);
-			if (node->next != NULL || i < size - 1)
-			{
+			if (pair == true)
 				printf(", ");
-				comma = 1;
-			}
+			printf("'%s': '%s'", node->key, node->value);
+			pair = true;
 			node = node->next;
 		}
 	}
-	if (comma)
-		printf("\b\b");
 	printf("}\n");
 }
